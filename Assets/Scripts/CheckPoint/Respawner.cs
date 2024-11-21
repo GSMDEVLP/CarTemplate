fa82bc8b3a19cfef14b3f0e01596bb7da81d087b
GSMDEVLP <salavatgabdullin068@gmail.com>
2024-11-21 18:17:17 +0300

    Добавил логику ухода с главной дороги

--- Assets/Scripts/CheckPoint/Respawner.cs
+++ Assets/Scripts/CheckPoint/Respawner.cs
@@ -1,15 +1,56 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 public class Respawner : MonoBehaviour
-{
+{ 
     private Transform _lastChecklPointPos;
 
-    private float _crashRotateAngle = 110f;
-    private float _safeDistanceForSafeMode = 5f;
+    private float _crashRotateAngle = 100f;
     private bool _isRespawning = false;
+    private float _rayDistance = 5f;
+
+    private int _roadLayer;
+    private bool _isOnRoad = true;
+    private float _warningTime = 5f;
+    private float _outOfRoadTimer = 0f;
 
     public static Action OnRespawnCar;
+
+
+    private void Awake()
+    {
+        _roadLayer = LayerMask.GetMask("Road");
+    }
+    private void FixedUpdate()
+    {
+        CheckIfOnRoad();
+        Respawn();
+    }
+
+    void CheckIfOnRoad()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, _rayDistance, _roadLayer))
+        {
+            Debug.Log(hit.collider.gameObject.layer);
+            _isOnRoad = true;
+            _outOfRoadTimer = 0f;
+        }
+        else
+        {
+            _isOnRoad = false;
+            _outOfRoadTimer += Time.deltaTime;
+            var waitTime = 0;
+            if (_outOfRoadTimer >= _warningTime && !_isRespawning)
+            {
+                Debug.Log("Респавн - съехал с дороги");
+                StartCoroutine(RespawnCar(waitTime));
+                OnRespawnCar?.Invoke();
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,31 +61,24 @@ public class Respawner : MonoBehaviour
         }
     }
 
-
-
-    private void FixedUpdate()
-    {
-        Respawn();
-    }
-
     private void Respawn()
     {
         float zRotation = transform.eulerAngles.z;
 
         if (zRotation > 180) zRotation -= 360;
-
+        var waitTime = 2;
         if (Mathf.Abs(zRotation) > _crashRotateAngle && !_isRespawning)
         {
+            Debug.Log("Респавн - перевернулся");
+            StartCoroutine(RespawnCar(waitTime));
             OnRespawnCar?.Invoke();
-            Debug.Log("Респавн");
-            StartCoroutine(RespawnCar());
         }
     }
-    private IEnumerator RespawnCar()
+    private IEnumerator RespawnCar(int waitTime)
     {
         _isRespawning = true;
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(waitTime);
 
         if (_lastChecklPointPos != null)
         {
